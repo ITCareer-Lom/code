4cca9ee4b81d965bdf732a5ac524cd84deccda98
tsvetelia <61344220+tsvetelia@users.noreply.github.com>
2020-04-26 18:08:12 +0300

    Items
    Всичко съм направила без стойността на предметите Необходими за Друго, защото не се сещам как да ги направя . Както и нямат стойност някои предмети и за това съм ги оставила, когато бъдат добавени.

--- TheGame/TheGameMVC/Model/Characters/Item.cs
+++ TheGame/TheGameMVC/Model/Characters/Item.cs
@@ -8,5 +8,53 @@ namespace TheGameMVC.Model.Characters
 {
     public class Item
     {
-    }
+        public class ItemGroupPower
+        {
+          
+            string name = "Sword";
+            string name = "Magic Stick";
+            string name = "Gun";
+            string name = "Cannon";
+            string name = "Axe";
+
+           
+            int Sword;
+            int MagicStick =  +55;
+            int Gun;
+            int Cannon = +65;
+            int Axe = +70;
+
+      
+        }
+        public class ItemGroupHealth
+        {
+            string name = "Armor";
+            string name = "Medicinal Decoction";
+            string name = "Herbs";
+            string name = "Food";
+
+            int Armor;
+            int MedicinalDecoction = +50;
+            int Herbs = +10;
+            int Food = +25;
+        }
+        public class ItemGroupGold
+        {
+            string name = "Purse with Money";
+            string name = "Gold";
+            string name = "Silver";
+
+            int PurseWithMoney = +200;
+            int Gold = +400;
+            int Silver;
+        }
+        public class ItemGroupOther
+        {
+            string name = "Holy water";
+            string name = "Spike";
+            string name = "Book with Magic";
+
+           
+        }
+    } 
 }
