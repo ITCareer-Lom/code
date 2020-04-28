8560f052d2f208266058ae54db02f5648eb8c4c2
vivi773 <61344401+vivi773@users.noreply.github.com>
2020-04-28 16:36:29 +0300

    Update Creature.cs
    Неща, които са направени:
    	Клас Creature
    	Свойства
    	Конструктор
    	методи
    		- IsDead
    		- IsVictimOf
    
    Неща, които НЕ са направени:
    	метода WonVictoryOver
    		- LoseItem на victim
    
    	метода Fight
    		- за победителя викаме WonVictoryOver()
    
    	метод Deal
    
    	метод CanHaveItem
    
    	метода AcquireItem
    		- увеличава нашите Health, Power, Gold
    		- намали нашия Gold (ако сме направили покупка)
    
    	метод LoseItem
    		- намалява нашите Health, Power, Gold
    
    	метод Validate
    		- проверка дали е валидно, ако не - хвърляме изключение
    		- да има име на съществото и то да е валидно име за играта
    		- да има здраве 100
    		- да има заредени първоначално само валидни предмети (проверяваме с CanHaveItem)

--- TheGame/TheGameMVC/Model/Characters/Creature.cs
+++ TheGame/TheGameMVC/Model/Characters/Creature.cs
@@ -8,5 +8,88 @@ namespace TheGameMVC.Model.Characters
 {
     public class Creature
     {
+        string Name; // името на съществото
+        int Health; // здравето му
+        int Power; // силата му
+        int Experience; // опита, който има
+        int Gold; // парите му
+        List Items; // предметите, които притежава
+        string Message; // съобщението, което носи това същество
+
+        public Creature(string name, int power, int experience, int gold, string message)
+        {
+            Name = name;
+            Health = 100;
+            Power = power;
+            Experience = experience;
+            Gold = gold;
+            Items = new List<Item>();
+            Message = message;
+        }
+
+        bool IsDead() // дали е умрял
+        {
+            Health <= 0;
+        }
+
+        bool IsVictimOf(Creature attacker) // дали може да бъде победен от друг
+        {
+            Health <= attacker.Power;
+        }
+
+        void WonVictoryOver(Creature victim) // какво става като победи друг            
+        {
+            Gold += victim.Gold;
+            Experience += victim.Experience;
+            AcquireItem(victim.Items);
+            // взима Gold, Experience и Items(с AcquireItem и LoseItem) на victim
+        }
+
+        void Fight(Creature opponent)
+        {
+            while (opponent.Health > 0 && Health > 0) // това продължава, докато един от двамата не умре 
+            {
+                opponent.Health -= Power; // бием се с някой намалява victim.Health със нашия Power 
+            }
+            // WonVictoryOver(opponent); за победителя викаме WonVictoryOver()
+        }
+
+        void Deal(Creature seller) // търгуваме с някой         
+        {
+            // намали нашия Gold (ако сме направили покупка)
+            // получаваме един или повече от Items(с AcquireItem и LoseItem) от seller
+        }
+
+        bool CanHaveItem(Item item) // дали това същество може да има този предмет
+        { }
+
+        void AcquireItem(Item item) // придобиваме предмет
+        {
+            if (CanHaveItem(item)) // първо проверяваме дали може да го притежаваме с CanHaveItem
+            {
+                Items.Add(item); // добавя се към нашите Items
+                // увеличава нашите Health, Power, Gold
+            }
+            else
+            {
+                throw new ArgumentException("You can't have this item!");
+                // (може и да не можем! - тогава хвърляме изключение или връщаме false)
+            }
+            // Може да намали нашия Gold(ако сме направили покупка)
+        }
+
+        void LoseItem(Item item) // разделяме се с предмет
+        {
+            // намалява нашите Health, Power, Gold
+            Items.Remove(item); // изтрива се от нашите Items
+        }
+
+        void Validate()
+        {
+            // проверка дали е валидно, ако не - хвърляме изключение
+            // да има име на съществото и то да е валидно име за играта
+            // да има здраве 100
+            // да има заредени първоначално само валидни предмети(проверяваме с CanHaveItem)
+        }
     }
 }
