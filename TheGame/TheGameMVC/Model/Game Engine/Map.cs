2dee19df628d65b6b617eec8b625bb41d64c1cee
Kristina929 <61344208+Kristina929@users.noreply.github.com>
2020-05-01 13:24:25 +0300

    Revert "Клас Hero и Card"
    This reverts commit 7cb5acfeb1e73888de6c7a3e48222f63115f7b13.

--- TheGame/TheGameMVC/Model/Game Engine/Map.cs
+++ TheGame/TheGameMVC/Model/Game Engine/Map.cs
@@ -8,51 +8,5 @@ namespace TheGameMVC.Model.Game_Engine
 {
     public class Map
     {
-        private List Levels;
-        private List Heroes;
-
-        public List levels{get;set;}
-        public List Heroes{get;set;}
-
-        public Map()
-        {
-            this.Levels = new List<Levels>();
-            this.Heroes = new List<Heroes>();
-        }
-        void Validate() 
-        {
-            if (Heroes <= 0)
-	        {
-                throw new ArgumentOutOfRangeException("There must be some heroes!");
-	        }
-
-            if (Hero.Validate() == true)
-	        {
-                return("It's okay.");
-	        }
-            else
-            {
-                throw new ArgumentOutOfRangeException("Heroes are not validate!");
-            }
-
-            if (Levels <= 0)
-	        {
-                throw new ArgumentOutOfRangeException("There must be some levels!");
-	        }
-
-            if (Level.Validate == true)
-	        {
-                return("It's okay.");
-	        }
-            else
-            {
-                throw new ArgumentOutOfRangeException("Levels are not validate!");
-            }
-        }
-        // проверка дали е валидна, ако не - хвърляме изключение
-        //да нямаме никакви герои
-        //дали героите са валидни
-        //да нямаме никакви нива
-        //дали всяко от нивата е валидно
     }
 }
