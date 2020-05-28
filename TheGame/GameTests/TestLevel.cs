081f152be3d786d2d32bf4e45b316968d5258759
tsvetelia <61344220+tsvetelia@users.noreply.github.com>
2020-05-28 16:44:45 +0300

    Create TestLevel.cs

---
+++ TheGame/GameTests/TestLevel.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheGameMVC.Model.Characters;
+
+namespace GameTests
+{
+    [TestFixture]
+    public class TestLevel
+    {
+        [Test]
+        public void TestLevelFinished()
+        {
+            //тестова ситуация
+            const int LevelId = 1;
+            var enemy = new Enemy() { Name = "Dragon", Id = 0 };
+            // var hero = new Hero() { Name = "Knight", Id = LevelId };
+
+            // извикване на метода
+            // level.IsCompleted();
+
+            // проверка дали е коректна ситуацията
+            Assert.That(Level.Id,
+                        Is.EqualTo(LevelId),
+                        "The level is not finished");
+        }
+    }
+}
