beb49d81002807bfd8f704370eb9fb0c8d1e8d1e
Nikola Viktorov <40743601+NikolaViktorov@users.noreply.github.com>
2020-05-30 23:08:57 +0300

    Updated Database

--- TheGame/TheGameMVC/Database/GameContext.cs
+++ TheGame/TheGameMVC/Database/GameContext.cs
@@ -6,35 +6,44 @@
 
     public class GameContext : DbContext
     {
+        internal static string ConnectionString = @"data source=(LocalDB)\MSSQLLocalDB;initial catalog = Game; integrated security = True; MultipleActiveResultSets=True;App=EntityFramework";
         public GameContext() { }
 
-        public GameContext(DbContextOptions options)
-        : base(options) { }
+        public GameContext(DbContextOptions options) : base(options) { }
 
         public DbSet<Hero> Heros { get; set; }
-
         public DbSet<Enemy> Enemies { get; set; }
-
         public DbSet<Item> Items { get; set; }
-
         public DbSet<Level> Levels { get; set; }
-
         public DbSet<Map> Maps { get; set; }
-
         public DbSet<Helper> Helpers { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder
-                    .UseSqlServer(Configuration.ConnectionString);
+                optionsBuilder.UseSqlServer(ConnectionString);
             }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-           
+            //modelBuilder.Entity<Level>().HasMany(l => l.Enemies).WithOne(e => e.Level).HasForeignKey(e => e.LevelId).OnDelete(DeleteBehavior.NoAction); 
+            //modelBuilder.Entity<Level>().HasMany(l => l.Helpers).WithOne(h => h.Level).HasForeignKey(e => e.LevelId).OnDelete(DeleteBehavior.NoAction); 
+            //modelBuilder.Entity<Creature>().HasMany(h => h.Items).WithOne(i => i.Owner).HasForeignKey(i => i.OwnerId).OnDelete(DeleteBehavior.SetNull);
+        }
+
+        public void ResetDatabase(bool shouldDropDatabase = false)
+        {
+            if (shouldDropDatabase)
+            {
+                Database.EnsureDeleted();
+            }
+
+            if (Database.EnsureCreated())
+            {
+                return;
+            }
         }
     }
 }
--- TheGame/TheGameMVC/Model/Game Engine/Level.cs
+++ TheGame/TheGameMVC/Model/Game Engine/Level.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace TheGameMVC.Model.Game_Engine
 {
     public class Level
     {
+        [Key]
+        public int LevelId { get; set; }
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Enemy> Enemies { get; set; }
