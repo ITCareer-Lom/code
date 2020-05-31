836feeb06ed382f0fcc3e577fee8e22422d5ec3e
Danail Manolov <1045522+dmanolov@users.noreply.github.com>
2020-05-31 09:22:12 +0300

    махнати пробите на Ники

--- TheGame/TheGameMVC/StartUp.cs
+++ TheGame/TheGameMVC/StartUp.cs
@@ -38,15 +38,10 @@
 
         static void Main(string[] args)
         {
-            Game game = new Game(new Map());
-            game.MyHero = new Hero() { Health = 10, Power = 10, Experience = 0 };
-            game.Opponent = new Creature() { Health = 100, Power = 100, Experience = 0 };
-
-            // game.Play(HeroActionType.Fight);
-
+           
             Deserializer ds = new Deserializer();
             var map = JsonToMap("V Map");
-            /*using (var context = new GameContext())
+            using (var context = new GameContext())
             {
                 ResetDatabase(context, shouldDropDatabase: true);
 
@@ -63,7 +58,7 @@
                 }
 
                 
-            }*/
+            }
             ConsoleGameController controler = new ConsoleGameController(map);
         }
 
